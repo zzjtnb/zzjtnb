47a811a9ff131087ee33ea3565048661ff41eae6
争逐 <zhengzhujituan@163.com>
2025-04-05 18:47:07 +0800

    去掉测试

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,9 +1,9 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 import UnoCSS from 'unocss/vite'
-import {defineConfig} from 'vitepress'
-import {Generator} from './plugins/auto'
+import { defineConfig } from 'vitepress'
+import { Generator } from './plugins/auto'
 
 // import {nav, sidebar} from './plugins/nav'
 
@@ -19,8 +19,8 @@ export default async () => {
     exclude: ['components', 'public'],
     debug: false,
   })
-  fs.writeFileSync('test/json/vitepress.json', JSON.stringify({nav: generator.nav, sidebar: generator.sidebar}, null, 2))
-  fs.writeFileSync('test/json/tree.json', JSON.stringify(generator?.fileTree || {}, null, 2))
+  // fs.writeFileSync('test/json/vitepress.json', JSON.stringify({nav: generator.nav, sidebar: generator.sidebar}, null, 2))
+  // fs.writeFileSync('test/json/tree.json', JSON.stringify(generator?.fileTree || {}, null, 2))
 
   return defineConfig({
     base: '/', // 部署站点的基础路径 默认为 '/'
@@ -34,13 +34,13 @@ export default async () => {
     cleanUrls: true,
     lastUpdated: true,
     locales: {
-      root: {label: '简体中文', lang: 'zh-Hans-CN'},
+      root: { label: '简体中文', lang: 'zh-Hans-CN' },
     },
 
     markdown: {
       // https://shiki.tmrs.site/themes
       // theme: { light: 'github-light', dark: 'github-dark' },
-      theme: {light: 'github-light', dark: 'slack-dark'},
+      theme: { light: 'github-light', dark: 'slack-dark' },
       lineNumbers: true,
       image: {
         // 默认禁用图片懒加载
@@ -77,7 +77,7 @@ export default async () => {
       // logo: 'images/icons/logo.svg',
       nav: generator.nav,
       sidebar: generator.sidebar,
-      socialLinks: [{icon: 'github', link: 'https://github.com/zzjtnb'}],
+      socialLinks: [{ icon: 'github', link: 'https://github.com/zzjtnb' }],
       search: {
         provider: 'local',
         options: {
@@ -128,42 +128,42 @@ export default async () => {
       skipToContentLabel: '跳转到内容',
     },
     head: [
-      ['link', {rel: 'icon', href: '/images/icons/logo.svg ', type: 'image/svg+xml'}],
+      ['link', { rel: 'icon', href: '/images/icons/logo.svg ', type: 'image/svg+xml' }],
       // 设置字符编码
-      ['meta', {charset: 'utf-8'}],
+      ['meta', { charset: 'utf-8' }],
 
       // 设置作者
-      ['meta', {name: 'Author', content: '争逐'}],
+      ['meta', { name: 'Author', content: '争逐' }],
 
       // 设置关键词
-      ['meta', {name: 'keywords', content: '争逐,编程,博客,政治,经济,军事,历史,天文,地理,人文,哲学'}],
+      ['meta', { name: 'keywords', content: '争逐,编程,博客,政治,经济,军事,历史,天文,地理,人文,哲学' }],
 
       // 设置版权信息
-      ['meta', {name: 'Copyright', content: '争逐版权所有'}],
+      ['meta', { name: 'Copyright', content: '争逐版权所有' }],
 
       // 设置搜索引擎爬虫规则
-      ['meta', {name: 'robots', content: 'all'}],
+      ['meta', { name: 'robots', content: 'all' }],
 
       // 启用移动端 Web App 模式
-      ['meta', {name: 'mobile-web-app-capable', content: 'yes'}],
-      ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+      ['meta', { name: 'mobile-web-app-capable', content: 'yes' }],
+      ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
 
       // 设置应用名称
-      ['meta', {name: 'application-name', content: '争逐'}],
-      ['meta', {name: 'apple-mobile-web-app-title', content: '争逐'}],
+      ['meta', { name: 'application-name', content: '争逐' }],
+      ['meta', { name: 'apple-mobile-web-app-title', content: '争逐' }],
 
       // 设置主题颜色
-      ['meta', {name: 'theme-color', content: '#2d3748'}],
-      ['meta', {name: 'msapplication-navbutton-color', content: '#2d3748'}],
+      ['meta', { name: 'theme-color', content: '#2d3748' }],
+      ['meta', { name: 'msapplication-navbutton-color', content: '#2d3748' }],
 
       // 设置 iOS 状态栏样式
-      ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent'}],
+      ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }],
 
       // 设置启动 URL
-      ['meta', {name: 'msapplication-starturl', content: '/'}],
+      ['meta', { name: 'msapplication-starturl', content: '/' }],
 
       // 设置视口
-      ['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no'}],
+      ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' }],
     ],
   })
 }
